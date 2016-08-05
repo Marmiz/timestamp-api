6a84276f5c7945ec2972790a659d4cb1db977093
claudio restifo <claudio.restifo@gmail.com>
2016-08-05 11:38:46 +0000

    changed server fucntionality

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/:timestamp', function(req, res){
+  var time = req.params.timestamp;
+  res.send(time);
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
