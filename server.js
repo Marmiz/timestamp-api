210cd8a591c58e7e98854d02891f7f985f567485
claudio restifo <claudio.restifo@gmail.com>
2016-08-29 07:25:08 +0000

    added momentum, and updated server fuctionailty

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
+var moment = require('moment');
+var path = require('path');
+
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/:timestamp', function(req, res){
-  var time = req.params.timestamp;
-  res.send(time);
+  var time = moment(req.params.timestamp, 'MMMM DD YYYY', true);
+  if (!time.isValid())
+    time = moment.unix(req.params.timestamp);
+    
+  if (!time.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }
+  
+  res.json({
+    'humanReadable': time.format('MMMM DD, YYYY'),
+    'unix': time.format('X')
+  });
 });
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
